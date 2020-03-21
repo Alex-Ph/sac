88e8a3648e6d42d51fb776bf61380c4656928653
Alex Ph <Alex.Philippi77@gmail.com>
2020-03-21 19:21:00 +0100

    remove target ... and fix endpoint

--- src/main/java/de/ph/sac/controller/ProcessDefinitionController.java
+++ src/main/java/de/ph/sac/controller/ProcessDefinitionController.java
@@ -1,10 +1,13 @@
 package de.ph.sac.controller;
 
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import de.ph.sac.controller.dto.SacProcessInstance;
 
 /**
  * Creating custom controller to avoid CORS and to be able to start our process
@@ -19,8 +22,13 @@ public class ProcessDefinitionController {
     private static final String SAC_KEY = "sacProcess";
 
     @PostMapping("/startSacProcessInstance")
-    public String startSacProcess() {
-        return runtimeService.startProcessInstanceByKey(SAC_KEY).getProcessInstanceId();
+    public SacProcessInstance startSacProcess() {
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(SAC_KEY);
+        
+        SacProcessInstance sacProcessInstance = new SacProcessInstance();
+        sacProcessInstance.setId(processInstance.getId());
+        
+        return sacProcessInstance;
     }
 
 }
---
+++ src/main/java/de/ph/sac/controller/dto/SacProcessInstance.java
@@ -0,0 +1,15 @@
+package de.ph.sac.controller.dto;
+
+public class SacProcessInstance {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
